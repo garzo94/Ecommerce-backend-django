67654d47274f666999e1b7180419f5ab7f0b1517
garzo94 <alexgarzo25@gmail.com>
2022-10-12 14:56:40 -0600

    adding .env

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sdfsdfsdf'
+os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 1
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ecommerce-backend-django-production.up.railway.app']
 
 
 # Application definition
@@ -168,8 +171,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'AKIAT2DPK7XMX7OEFYEU'
-AWS_SECRET_ACCESS_KEY =  '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'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY =  os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'drumshop'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
@@ -187,4 +190,3 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-print(os.environ.get('SECRET_KEY'),'SECREEEEET')
