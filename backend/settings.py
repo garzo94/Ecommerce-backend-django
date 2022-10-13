6921a9b1955e04976ae8286881bc7cc6ef51ad08
garzo94 <alexgarzo25@gmail.com>
2022-10-13 09:00:58 -0600

    cors origins

--- backend/settings.py
+++ backend/settings.py
@@ -184,7 +184,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS: True
 CORS_ALLOWED_ORIGINS = [
     "https://example.com",
-    "https://sub.example.com",
+    "https://main--glittering-longma-c3df71.netlify.app",
     "http://localhost:8080",
     "http://127.0.0.1:5173",
 ]
