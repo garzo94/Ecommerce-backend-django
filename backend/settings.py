1b1bf0b5ea9e1913a7a4b8a238ceae69ec55ea31
garzo94 <alexgarzo25@gmail.com>
2022-11-20 16:04:18 -0600

    sdf

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY')
 SECRET_KEY = "sdfsdf"
-# CSRF_TRUSTED_ORIGINS = ['https://ecommerce-backend-django-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://ecommerce-backend-django-production.up.railway.app']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
