2b690f4369065ca293f6610c49e7e8cf7f538685
garzo94 <alexgarzo25@gmail.com>
2022-11-20 09:06:14 -0600

    methods

--- backend/settings.py
+++ backend/settings.py
@@ -54,9 +54,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-      "corsheaders.middleware.CorsMiddleware",
-        'django.middleware.security.SecurityMiddleware',
-
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -191,6 +190,15 @@ CORS_ALLOW_ALL_ORIGINS: True
 # CORS_ALLOWED_ORIGINS = [
 #     "*",
 # ]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
 
 
 
