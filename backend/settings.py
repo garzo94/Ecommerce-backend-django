71c8fb4f132d7da225330b24ee7f202be9d84bd8
garzo94 <alexgarzo25@gmail.com>
2022-11-19 18:23:23 -0600

    cors

--- backend/settings.py
+++ backend/settings.py
@@ -181,9 +181,9 @@ AWS_S3_FILE_OVERWRITE = False
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# CORS_ALLOW_ALL_ORIGINS: True
-CORS_ALLOWED_ORIGINS = [
-    "*",
-]
+CORS_ALLOW_ALL_ORIGINS: True
+# CORS_ALLOWED_ORIGINS = [
+#     "*",
+# ]
 
 
