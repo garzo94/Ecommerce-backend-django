0adc6eb2bd12b14a098450da6f899897a2dc108c
garzo94 <alexgarzo25@gmail.com>
2022-11-19 18:17:11 -0600

    *

--- backend/settings.py
+++ backend/settings.py
@@ -183,10 +183,7 @@ AWS_S3_FILE_OVERWRITE = False
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS: True
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://drumshope.netlify.app",
-    "http://localhost:8080",
-    "http://127.0.0.1:5173",
+    "*",
 ]
 
 
