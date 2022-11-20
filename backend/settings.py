f231184cd18d9807eada2916f116bcf05e6a2806
garzo94 <alexgarzo25@gmail.com>
2022-11-19 19:28:24 -0600

    que pedo

--- backend/settings.py
+++ backend/settings.py
@@ -54,11 +54,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+      "corsheaders.middleware.CorsMiddleware",
         'django.middleware.security.SecurityMiddleware',
-       "corsheaders.middleware.CorsMiddleware",
+
     "whitenoise.middleware.WhiteNoiseMiddleware",
-
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
